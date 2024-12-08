a8e774b7c7958e6ec69a906866ddce21d21e461f
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2024-12-08 16:39:39 -0600

    fix: remove derive on types

--- src/fcs/mod.rs
+++ src/fcs/mod.rs
@@ -5,12 +5,10 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 /// Metadata aliased as a hash-map with
-#[derive(Debug, Deserialize, Serialize)]
 pub type Metadata = HashMap<String, String>;
 
 /// EventData aliased as a hash-map with parameter IDs as strings and their
 /// event data as a vector of f64s.
-#[derive(Debug, Deserialize, Serialize)]
 pub type EventData = HashMap<String, Vec<f64>>;
 
 /// FCS sample object containing metadata and event data.
